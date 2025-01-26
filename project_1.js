15660cf24fc56c652586cf4e964c918c2f5fdc52
w3schools-test <test@w3schools.com>
2025-01-26 13:00:16 +0100

    fix the problem

--- project_1.js
+++ project_1.js
@@ -1,7 +1,13 @@
-var count=0;
-for (let i= 0; i < 100; i++) {
-    if (i%5==0 || i%3==0) {
-        count+=i;
-    }  
+
+function CalculateSumOfMultiples (count) {
+    var sum = 0;
+    for (let i= 0; i < 100; i++) {
+        if (i%5==0 || i%3==0) {
+            sum+=i;
+        }  
+    }
+    return sum;
 }
-console.log(count)
+
+var result = CalculateSumOfMultiples(100);
+console.log(CalculateSumOfMultiples(result))
